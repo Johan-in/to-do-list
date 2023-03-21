be744af1d181b4c9033bd25e4d97ebbaf19f34f4
Johan <senatovi21@gmail.com>
2023-03-21 22:27:02 +0300

    adding edit.addEvent

--- main.js
+++ main.js
@@ -96,5 +96,17 @@ function DisplayTodos () {
 
             DisplayTodos();
         })
+
+        edit.addEventListener('click', e => {
+            const input = content.querySelector('input');
+            input.removeAttribute('readonly');
+            input.focus();
+            input.addEventListener('blur', e => {
+                input.setAttribute('readonly', true);
+                todo.content = e.target.value;
+                localStorage.setItem('todos', JSON.stringify(todos));
+                DisplayTodos();
+            })
+        })
     })
 }
